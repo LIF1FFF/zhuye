2e309b7d2a7c4c5bed803c053b76e42445f31b83
FFF <76924577+LIF1FFF@users.noreply.github.com>
2026-06-25 18:55:40 +0800

    1

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+var iUp = (function () {
+    var t = 0,
+        d = 150,
+        clean = function () { t = 0; },
+        up = function (e) {
+            setTimeout(function () { $(e).addClass("up"); }, t);
+            t += d;
+        },
+        down = function (e) { $(e).removeClass("up"); },
+        toggle = function (e) {
+            setTimeout(function () { $(e).toggleClass("up"); }, t);
+            t += d;
+        };
+    return { clean: clean, up: up, down: down, toggle: toggle };
+})();
+
+$(document).ready(function () {
+    $(".iUp").each(function (i, e) { iUp.up(e); });
+    $(".js-avatar")[0].onload = function () { $(".js-avatar").addClass("show"); };
+});
